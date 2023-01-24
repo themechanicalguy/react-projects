30635cf511a9bd94372c9e2e8995c64250d5d646
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-01-24 22:53:33 +0530

    revision

--- car-search/pics/src/api.js
+++ car-search/pics/src/api.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
-const SearchImage = async () => {
+const SearchImage = async (term) => {
+  console.log(term);
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID JlqpCgv1x2Mu90nEZq_IGEbcUmmPjE2d7Z1pb-Za3j8",
     },
     params: {
-      query: "cars",
+      query: term,
     },
   });
 
   console.log(response);
 
-  return response;
+  return response.data.results;
 };
 
 export default SearchImage;
